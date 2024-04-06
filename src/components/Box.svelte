b0121f220c4a6257af0d648b712ac1cfaecfadc7
Nurman2021 <sitihajarhzamzah@student.unismuh.ac.id>
2024-04-06 07:04:22 -0700

    modify loader 3JS

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -2,9 +2,8 @@
     import { onMount } from 'svelte';
     import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
     import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+    let mixers = [];
     let mixer;
-    let smolhome_action;
-    
     
     
     onMount(async ()=>{
@@ -15,8 +14,6 @@
             const renderer = new THREE.WebGLRenderer({antialias: true, canvas: document.querySelector('#bg')});
             const SmolLoader = new GLTFLoader();
             const controls = new OrbitControls(camera, renderer.domElement);
-
-        
 
             renderer.setSize(window.innerWidth, window.innerHeight);
             renderer.setClearColor(0x000000, 0);
@@ -41,6 +38,17 @@
             const grid = new THREE.GridHelper(100, 100);
             // scene.add(grid);
 
+            
+            const points = [
+                new THREE.Vector3(-10, 0, 10),
+                new THREE.Vector3(-5, 5, 5),
+                new THREE.Vector3(0, 0, 0),
+                new THREE.Vector3(5, -5, 5),
+                new THREE.Vector3(10, 0, 10)
+
+            ];
+
+            const path = new THREE.CatmullRomCurve3(points);
 
             const planeGeo = new THREE.PlaneGeometry(100, 100);
             const PlaneMath = new THREE.MeshPhongMaterial({color: 0x58adbf, dithering: true});
@@ -49,11 +57,13 @@
             plane.rotation.x = -0.5 * Math.PI;
             scene.add(plane);
 
-            SmolLoader.load('src/model/smol-home/scene.gltf', gltf =>{
+            function loadModel(modelPath, position){
+                SmolLoader.load(modelPath, gltf =>{
                 const model = gltf.scene;
                 const animations = gltf.animations;
 
-                model.position.set(0.9, 0.04, -0.1);
+                // model.position.set(0.9, 0.04, -0.1);
+                model.position.copy(position);
                 model.rotation.y = -1.02;
 
                 model.traverse(function(node){
@@ -68,74 +78,31 @@
                 console.log('home',model);
 
                 mixer = new THREE.AnimationMixer(model);
+
+                mixers.push(mixer);
                 animations.forEach((clip) => {
                     mixer.clipAction(clip).play();
                 });
                
 
             });
+            }
+            loadModel('src/model/smol_ame_-_amelia_watson_holomyth/scene.gltf', new THREE.Vector3(0, 0, 0));
+            loadModel('src/model/smol-home/scene.gltf', new THREE.Vector3(0.9, 0.04, -0.1));
 
-            // SmolLoader.load('src/model/smol_ame_-_amelia_watson_holomyth/scene.gltf', gltf =>{
-            //     const model = gltf.scene;
-            //     const animations = gltf.animations;
+            
 
-            //     model.position.set(0, 0, 0);
-            //     model.rotation.y = -1.02;
-
-            //     model.traverse(function(node){
-            //         if(node.isMesh)
-            //         node.castShadow = true;
-
-            //     })
-            //     model.castShadow = true;
-            //     model.receiveShadow = true;
-
-            //     scene.add(model);
-            //     console.log('ame',model);
-
-            //     mixer = new THREE.AnimationMixer(model);
-            //     animations.forEach((clip) => {
-            //         mixer.clipAction(clip).play();
-            //     });
-               
-
-            // });
-
-
-            // SmolLoader.load('src/model/room_diorama_model/scene.gltf', gltf =>{
-            //     const model = gltf.scene;
-            //     const animations = gltf.animations;
-
-            //     model.position.set(0, 0, 0);
-            //     model.rotation.y = -1.02;
-
-            //     model.traverse(function(node){
-            //         if(node.isMesh)
-            //         node.castShadow = true;
-
-            //     })
-            //     model.castShadow = true;
-            //     model.receiveShadow = true;
-
-            //     scene.add(model);
-            //     console.log('diorama',model);
-
-            //     mixer = new THREE.AnimationMixer(model);
-            //     animations.forEach((clip) => {
-            //         mixer.clipAction(clip).play();
-            //     });
-               
-
-            // });
-
-
-
+            function updateAnimation(deltaTime){
+                for (let i = 0; i<mixers.length; i++){
+                    mixers[i].update(deltaTime);
+                }
+            }
 
             const animate = function (){
                 requestAnimationFrame(animate);
-                const delta = clock.getDelta();
+                const deltaTime = clock.getDelta();
 
-                mixer.update(delta);
+                updateAnimation(deltaTime);
                 
 
                 renderer.render(scene, camera);
